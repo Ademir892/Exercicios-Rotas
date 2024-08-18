e57119ca7d40c613f8bf98731d797e6e0158e4e6
Ademir Traesel <ademirtraesel@hotmail.com>
2024-08-18 16:05:01 -0300

    Exerc 1 finalized

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
-import '../Header/styles.css';
+import { Link } from "react-router-dom";
+import "../Header/styles.css";
 
-export default function Header(){
-    return(
-        <header>
-            <div className="container">
-                <h1 className='site-name'>MeuSite</h1>
-            </div>
-        </header>
-    );
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1 className="site-name">MeuSite</h1>
+        </Link>
+      </div>
+    </header>
+  );
 }
